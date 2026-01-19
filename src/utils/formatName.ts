9c07051a0da2042cd3c832719637718afa3ebf6b
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2026-01-19 12:50:19 -0500

    fix(utils): prevent forcing name to lowercase

--- src/utils/formatName.ts
+++ src/utils/formatName.ts
@@ -3,7 +3,6 @@ export function formatName(text: string) {
   return text
     .trim()
     .replace(/\s+/g, ' ')
-    .toLowerCase()
     .split(' ')
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ');
