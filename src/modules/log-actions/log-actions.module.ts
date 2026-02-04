5439e63d7454cc000b1b3930881176ed33271dea
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2026-02-03 23:05:50 -0500

    feat(log-actions): add endpoint to retrieve action logs
    - Implemented GET /log-actions endpoint
    - Added pagination for log entries
    - Included user details in log data
    - Fetched related entity names
    - Secured endpoint with JWT authentication

--- src/modules/log-actions/log-actions.module.ts
+++ src/modules/log-actions/log-actions.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { LogActionsService } from './log-actions.service';
 import { LogActionsInterceptor } from './log-actions.interceptor';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { LogActionsController } from './log-actions.controller';
 
 @Module({
   imports: [PrismaModule],
   providers: [LogActionsService, LogActionsInterceptor],
   exports: [LogActionsService],
+  controllers: [LogActionsController],
 })
 export class LogActionsModule {}
